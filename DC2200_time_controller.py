e0ff8b16aa4cd31af4898d95165d62b2795b2ec3
wattsc2 <wattsc2@oregonstate.edu>
2021-07-02 15:33:01 -0700

    fixed issue where program was being run without clicking on the button. Found that it was the parenthesis after run() that was causing it to be run during the init step. Also made it to where the only thing that can be input is numbers.

--- DC2200_time_controller.py
+++ DC2200_time_controller.py
@@ -1,17 +1,22 @@
 # imports
 import sys
-from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.QtCore import pyqtSlot,Qt
+from PyQt5.QtWidgets import QApplication, QDialog,QMessageBox
 from PyQt5.uic import loadUi
 import pyvisa
 import time
 from pyvisa.resources import MessageBasedResource
+from qtpy import QtCore
+
+if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
 
 class DC2200_controller(QDialog):
     def __init__(self):
         super(DC2200_controller, self).__init__()
         loadUi('DC2200_brightness.ui', self)
-        self.run_button.clicked.connect(self.run())
+        self.run_button.clicked.connect(self.run)
+        self.show()
 
         # get the LED resource loaded.
         rm = pyvisa.ResourceManager()
@@ -23,14 +28,22 @@ class DC2200_controller(QDialog):
         self.DC2200.write("OUTP:STAT ON")
         self.DC2200.write("SOUR:CBR:BRIG 100")
 
-
-
     @pyqtSlot()
     def run(self):
-        pre_illumination_time = int(self.DarkTimeLine.currentText())  # in seconds
-        illumination_time = int(self.IlluminationTimeLine.currentText())   # illumination time in seconds
-        time_between_samples = int(self.StepSizeLine.currentText())   # in seconds
-        step_size = illumination_time // time_between_samples
+        pre_illumination_time = self.DarkTimeLine.text()  # in seconds
+        illumination_time = self.IlluminationTimeLine.text()   # illumination time in seconds
+        time_between_samples = self.StepSizeLine.text()  # in seconds
+
+        try:
+            pre_illumination_time = float(pre_illumination_time)
+            illumination_time = float(illumination_time)
+            time_between_samples = float(time_between_samples)
+
+        except Exception:
+            QMessageBox.about(self, 'Error', 'Input can only be an number')
+            pass
+
+        step_size = illumination_time// time_between_samples
 
         # general set up and warm up of the LED
         self.DC2200.write("SOUR:MOD 2")
@@ -68,8 +81,8 @@ class DC2200_controller(QDialog):
 
         self.DC2200.write("OUTP:STAT OFF") # turns the LED off
 
-
-app = QApplication(sys.argv)
-widget = DC2200_controller()
-widget.show()
-sys.exit(app.exec_())
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    widget = DC2200_controller()
+    widget.show()
+    sys.exit(app.exec_())
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DC2200-Time-Controlled-Experiment
 This is for an experimental program to control a Thorlabs DC2200's brightness level over time.
+
+pyuic5 is used to create the SetUpUI.py file although neither is being used at this moment.
